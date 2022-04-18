5741f16c196e97402180a1c229c5c810aa4a4a5b
djay <djay@boegismedia.com>
2022-04-18 23:43:18 +0700

    create endpoint to receive an event and use const for the port

--- index.js
+++ index.js
@@ -2,27 +2,36 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const { randomBytes } = require('crypto')
+const axios = require('axios')
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
 const posts = {};
-
+const eventBusEndpoint = 'http://localhost:4005/events'
 app.get('/posts', (req, res) => {
   res.send(posts)
 });
 
-app.post('/posts', (req, res) => {
+app.post('/posts', async (req, res) => {
   const id = randomBytes(4).toString('hex');
   const {title} = req.body;
   posts[id] = {
     id, title
   };
 
+  await axios.post(eventBusEndpoint, {type:'PostCreated', data:posts[id]})
   res.status(201).send(posts[id])
 });
 
-app.listen(4000, () => {
-  console.log('Listening on 4000')
+app.post('/events', (req, res) => {
+  const {type} = req.body
+  console.log(`receiving ${type} event`)
+  res.send({})
+})
+
+const port = 4000
+app.listen(port, () => {
+  console.log(`Listening on ${port}`)
 });
